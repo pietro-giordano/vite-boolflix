abc6da2d2755a7e3f65f351d90576be53df9a4e9
Pietro Giordano <giopi87@gmail.com>
2023-02-08 15:38:53 +0100

    inizializzazione

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from './store';
+
+export default {
+      name: 'App',
+      components: {
+
+      },
+      data() {
+            return {
+                  store
+            }
+      }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@import './styles/main.scss';
+</style>
